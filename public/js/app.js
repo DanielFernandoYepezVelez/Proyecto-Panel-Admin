034cd337e7722a29496a5424ef6242904cc54d9b
Daniel Fernando Yepez Velez <danipez.02@gmail.com>
2020-04-26 21:26:10 -0500

    Agregando Interactividad Desde JS

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,28 @@
+const containerMain = document.getElementById('app');
+const nav = document.getElementById('nav');
 const barra = document.getElementById('lateral');
+const form = document.getElementById('form');
 
 const ocultarMenu = (e) => {
     e.preventDefault();
 
     if (e.target.classList.contains('left')) {
-        barra.classList.toggle('lateral-menu');
-        console.log('flecha izquierda');
+        app.classList.toggle('main-container-two');
+        form.classList.toggle('new-form');
+        barra.classList.toggle('lateral-visible');
+
+        e.target.style.display = 'none';
+        e.target.nextElementSibling.style.display = 'block';
+        nav.style.display = 'block';
+
     } else if (e.target.classList.contains('right')) {
-        console.log('flecha derecha');
+        app.classList.toggle('main-container-two');
+        form.classList.toggle('new-form');
+        barra.classList.toggle('lateral-visible');
+
+        e.target.style.display = 'none';
+        e.target.previousElementSibling.style.display = 'block';
+        nav.style.display = 'none';
     }
 }
 
